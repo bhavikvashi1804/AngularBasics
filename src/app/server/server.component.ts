809e9920eaad5212dad5473224ccdaa9364b2602
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-03 20:08:28 +0530

    Template Way 2: External template

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  template: `
-  <h1>Bhavik Vashi</h1>
-  <h2>CTS PAT</h2>
-  `,
+  templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
