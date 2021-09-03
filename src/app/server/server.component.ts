d742d4b8aceebaf9e0368afb6aa61feae312c078
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-03 20:19:31 +0530

    Style Way 3: Inline Style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles: [
+    `
+      h2{
+        color: cyan;
+      }
+    `,
+    `h1{
+        color: grey;
+    }
+    `
+  ]
 })
 export class ServerComponent implements OnInit {
 
