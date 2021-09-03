0b923a59858e21c69f0b64e05d1675ddf8dd424c
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-03 20:06:55 +0530

    Template Way 1: Inline Template

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
+  template: `
+  <h1>Bhavik Vashi</h1>
+  <h2>CTS PAT</h2>
+  `,
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
