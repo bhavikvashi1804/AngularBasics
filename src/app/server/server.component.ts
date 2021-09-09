4eac3adce7c6471e0c2a650253f0909a5377ec93
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-09 13:53:50 +0530

    ngFor use-case

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  name = "";
+  studentNames = ['Raj', 'Yash', 'Bhavik', 'Meet', 'Dhruvik'];
   
   constructor() {}
 
