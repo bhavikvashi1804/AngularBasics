db9a978fbe4f3b9e6039f27c1b505e3a4eb44c43
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-11 11:03:35 +0530

    Give alias name to Custom Prop Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  @Input()
+  @Input('serverData')
   serverElement!: {
     name: string,
     type: string,
