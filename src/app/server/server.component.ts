51139b3425cf88aa07af005d39bb349feb7d8886
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-11 10:20:04 +0530

    display the list of server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  serverElements!: {
+  serverElement!: {
     name: string,
     type: string,
     content: string
