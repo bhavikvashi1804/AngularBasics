2800a60eec2abbe971038e6ccee78d01d177c369
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-11 10:28:09 +0530

    Custom Prop Binding: Parent to Child Data Pass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  @Input()
   serverElement!: {
     name: string,
     type: string,
