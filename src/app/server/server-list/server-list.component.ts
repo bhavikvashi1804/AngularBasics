fca9e3177a4cff5bb8dd8b075e5eccbedfbfdcc6
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-13 21:33:45 +0530

    ng-content and @ContentChild() to access projecting content using local ref var

--- src/app/server/server-list/server-list.component.ts
+++ src/app/server/server-list/server-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-list',
   templateUrl: './server-list.component.html',
   styleUrls: ['./server-list.component.css']
 })
-export class ServerListComponent implements OnInit {
+export class ServerListComponent implements OnInit, AfterContentInit  {
 
   @Input('serverData')
   serverElement!: {
@@ -14,9 +14,17 @@ export class ServerListComponent implements OnInit {
     content: string
   };
 
+  @ContentChild('myPara')
+  paragraphElement!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    //console.log(this.paragraphElement.nativeElement);
+  }
+
+  ngAfterContentInit(){
+    console.log(this.paragraphElement.nativeElement);
   }
 
 }
